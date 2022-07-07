4a0e3661191f58c9cd72a269fb33b87c0809010e
Erwin <erwin@ErwindeMacBook-Pro.local>
2022-07-07 16:35:16 +0800

    scripts

--- run_metapart_cloud.sh
+++ run_metapart_cloud.sh
@@ -1,3 +1,3 @@
 cd src
 CUDA_VISIBLE_DEVICES=1 python main.py --outdir outdir --fitness latency --cstr buffer_size --num_pe 65536 --l1_size 65536 --l2_size 25165824 --epochs 300 --df eye --num_steps 6 --test_epochs 10
-cd ../../
+cd ../
--- run_metapart_edge.sh
+++ run_metapart_edge.sh
@@ -1,3 +1,3 @@
-cd src
-CUDA_VISIBLE_DEVICES=0 python main.py --outdir outdir --fitness latency --cstr buffer_size --num_pe 4096 --l1_size 4096 --l2_size 1572864 --epochs 300 --df dla --num_steps 6 --test_epochs 10
-cd ../../
+cd src2
+CUDA_VISIBLE_DEVICES=1 python main.py --outdir outdir --fitness latency --cstr buffer_size --num_pe 4096 --l1_size 4096 --l2_size 1572864 --epochs 300 --df dla --num_steps 6 --test_epochs 10
+cd ../
